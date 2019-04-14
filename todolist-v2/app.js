38e260a9411a5fe9c147b643c14f936a3ae4ab62
Edward Chao <EdwardChao@live.com>
2019-04-14 01:17:40 -0700

    Finish Web Development Course

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-edward:Test123@cluster0-hls0u.mongodb.net/todolistDB", { //replace <password> with admin-edward's password and test
   useNewUrlParser: true
 });
 
@@ -157,6 +157,11 @@ app.get("/:customListName", function(req, res) {
 //  res.render("about");
 //});
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully!");
 });
